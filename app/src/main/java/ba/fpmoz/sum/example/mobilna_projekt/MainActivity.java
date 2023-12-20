fbf4343f38f55d5217277ef8619db312295123d8
Cager <filip.crnogorac@fpmoz.sum.ba>
2023-12-20 09:06:59 +0100

    initial commit

---
+++ app/src/main/java/ba/fpmoz/sum/example/mobilna_projekt/MainActivity.java
@@ -0,0 +1,14 @@
+package ba.fpmoz.sum.example.mobilna_projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
